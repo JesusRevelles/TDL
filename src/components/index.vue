02ddcee3da6e532f72669639078f72d04ce69533
devdangel <ajguidet@gmail.com>
2018-06-05 21:45:31 +0200

    second commit

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,15 +1,18 @@
 <template>
 <div>
   <h2>TODO</h2>
-  <input type="text" :placeholder="place" v-model="accion">
+  <input type="text" @keyup.enter="addTarea()" :placeholder="place" v-model="accion">
   <input type="button" @click="addTarea()" value="AAA">
+  <input type="button" @click="callFirebase()" value="FIREBASE">
   <ul>
-    <li :class="{'done': tarea.done}" @click="deleteTarea(tarea, index)" v-for="(tarea,index) in tareas" :key="index">{{index+1}}-{{tarea.text}}<span v-if="tarea.done"> HECHO</span></li>
+    <li :class="{'done': tarea.done}" @click="deleteTarea(tarea, index)" v-for="(tarea,index) in todos" :key="index">{{index+1}}-{{tarea.text}}<span v-if="tarea.done"> HECHO</span></li>
   </ul>
+  {{todos}}
 </div>
 </template>
 
 <script>
+import {db} from '../plugins/firebase'
 export default {
   data() {
     return {
@@ -18,20 +21,48 @@ export default {
         {text: 'Aprender Vue', done: false},
         {text: 'Aprender ES6', done: true}
       ],
-      place: 'AÑADE'
+      place: 'AÑADE',
+      books: {},
+      todos: {}
     }
   },
   methods: {
     addTarea() {
-      this.tareas.push(this.accion)
+      this.tareas.push({
+        text: this.accion,
+        done: false
+      })
       this.accion = ''
     },
     deleteTarea(t,i) {
       setTimeout(() => {
         this.tareas.splice(i, 1)
       }, 1000)
+    },
+    callFirebase() {
+      const newTarea = {
+        text: this.accion,
+        done: false,
+        createdAt: new Date().toISOString()
+      }
+      if (this.accion != '') {
+        db.collection('todo')
+        .add(newTarea)
+        .then((data) => {
+          console.log(data)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+      }
+
     }
-  }
+  },
+    firestore() {
+      return {
+        todos: db.collection('todo'),
+      }
+    }
 }
 </script>
 
---
+++ src/plugins/firebase.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import VueFirestore from 'vue-firestore'
+
+Vue.use(VueFirestore)
+
+export const app = firebase.initializeApp({
+  apiKey: 'AIzaSyCcZU82iQ5L2odXt1wIuVrb4vWqJrwTQUY',
+  authDomain: 'todovue-c895e.firebaseapp.com',
+  databaseURL: 'https://todovue-c895e.firebaseio.com',
+  projectId: 'todovue-c895e'
+})
+
+// const ref = firebase.firestore()
+// const settings = {
+//   timestampsInSnapshots: true
+// }
+export const db = firebase.firestore()
+
+// export const db = firebase.firestore()
+export const usersCollection = firebase.firestore().collection('users')
+
+// export const messagesCollection = firebase.firestore().collection('messages');
+// export const typingStatus = firebase.firestore().collection('typing-status');
+// export const likesCollection = firebase.firestore().collection('likes');
