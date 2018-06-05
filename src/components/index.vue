3114a7a18d2b1e91f84592ef779a5ac15510c7df
JesusRevelles <sheevalum@gmail.com>
2018-06-05 21:03:59 +0200

    prueba
    prueba

---
+++ src/components/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>   
+        <h2>TODO</h2>
+        <input v-model="prueba"  type="text"> {{prueba}}
+    </div>
+</template>
+
+<script>
+export default{
+    data () {
+        return {
+            prueba: ""
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
