9fe6b77f70fe758000c8e239022989a1988f0b7f
JesusRevelles <sheevalum@gmail.com>
2018-06-05 23:03:20 +0200

    borrar

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,17 +1,14 @@
 <template>
 <div>
   <h2>TODO</h2>
-<<<<<<< HEAD
   <input type="text" @keypress.enter="addTarea()" :placeholder="place" v-model="accion">
-=======
-  <input type="text" @keyup.enter="addTarea()" :placeholder="place" v-model="accion">
->>>>>>> 02ddcee3da6e532f72669639078f72d04ce69533
   <input type="button" @click="addTarea()" value="AAA">
   <input type="button" @click="callFirebase()" value="FIREBASE">
   <ul>
-    <li :class="{'done': tarea.done}" @click="deleteTarea(tarea, index)" v-for="(tarea,index) in todos" :key="index">{{index+1}}-{{tarea.text}}<span v-if="tarea.done"> HECHO</span></li>
+    <li :class="{'done--block': tarea.done}" @click="deleteTarea(tarea, index)" v-for="(tarea,index) in todos" :key="index">{{index+1}}-{{tarea.text}}<span v-if="tarea.done"> HECHO</span></li>
   </ul>
   {{todos}}
+  {{tareas}}
 </div>
 </template>
 
@@ -26,34 +23,24 @@ export default {
         {text: 'Aprender ES6', done: true}
       ],
       place: 'AÑADE',
-      books: {},
       todos: {}
     }
   },
   methods: {
     addTarea() {
       this.tareas.push({
-<<<<<<< HEAD
-          text: this.accion,
-          done: false
-=======
         text: this.accion,
         done: false
->>>>>>> 02ddcee3da6e532f72669639078f72d04ce69533
       })
       this.accion = ''
     },
     deleteTarea(t,i) {
-<<<<<<< HEAD
         this.tareas[i].done = true;
-        setTimeout(() => {
-            this.tareas.splice(i, 1);
-        }, 4000)
-=======
       setTimeout(() => {
         this.tareas.splice(i, 1)
       }, 1000)
     },
+    /*
     callFirebase() {
       const newTarea = {
         text: this.accion,
@@ -72,19 +59,24 @@ export default {
       }
 
     }
+    */
   },
+    /*
     firestore() {
       return {
         todos: db.collection('todo'),
       }
->>>>>>> 02ddcee3da6e532f72669639078f72d04ce69533
     }
+    */
 }
 </script>
 
 <style>
-.done {
+.done--block {
   color: green
+}
+.done--none {
+  color: red
 }
 
 </style>
