52d86b3154d753b268646a9316561f2322d7293b
devdangel <ajguidet@gmail.com>
2018-06-05 21:03:36 +0200

    first commit todo

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <header>
+      <div id="logo"><img src="" alt=""></div>
+    </header>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/index.vue
@@ -0,0 +1,43 @@
+<template>
+<div>
+  <h2>TODO</h2>
+  <input type="text" :placeholder="place" v-model="accion">
+  <input type="button" @click="addTarea()" value="AAA">
+  <ul>
+    <li :class="{'done': tarea.done}" @click="deleteTarea(tarea, index)" v-for="(tarea,index) in tareas" :key="index">{{index+1}}-{{tarea.text}}<span v-if="tarea.done"> HECHO</span></li>
+  </ul>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      accion: '',
+      tareas: [
+        {text: 'Aprender Vue', done: false},
+        {text: 'Aprender ES6', done: true}
+      ],
+      place: 'AÑADE'
+    }
+  },
+  methods: {
+    addTarea() {
+      this.tareas.push(this.accion)
+      this.accion = ''
+    },
+    deleteTarea(t,i) {
+      setTimeout(() => {
+        this.tareas.splice(i, 1)
+      }, 1000)
+    }
+  }
+}
+</script>
+
+<style>
+.done {
+  color: green
+}
+
+</style>
